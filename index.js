8d2a5510f6ad87493b729000e9aabf80f268bfb4
Stephen Merki <stephen.merki232@gmail.com>
2023-05-19 15:51:23 -0500

    Set up server enviroment

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+const express = require('express');
 const dayjs = require('dayjs');
 const url_string = window.location;
 const url = new URL(url_string);
 const timeStamp = url.searchParams.get("timeStamp");
+
+const PORT = process.env.PORT || 80;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 function converter(timeStamp) {
     let converted = {
@@ -10,4 +19,12 @@ function converter(timeStamp) {
     return converted;
 }
 
-converter(timeStamp);
+// Set the homepage for the website
+app.get('/', (req, res) =>{
+    converter(timeStamp);
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.listen(PORT, () => 
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
