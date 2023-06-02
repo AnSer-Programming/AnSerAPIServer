0f1f9e98a7609f5c41299472ce21b21f7ff8ac73
Cristian <Crmueller1@users.noreply.github.com>
2023-06-02 15:24:14 -0500

    Create Cristian.js

---
+++ server/routes/api/Cristian.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+const var1 = "Hello, World";
+
+function timeConverter(timeZoneDif, sunRiseUnix, sunSetUnix) {
+    return router.send("This is Cristian's playground");
+}
+
+router.get('/:Cristian', (req, res) => {
+    //timeConverter(req.params.timeZoneDif, req.params.sunRiseUnix, req.params.sunSetUnix)
+    
+    res.send(timeConverter);
+});
+
+module.exports = router;
