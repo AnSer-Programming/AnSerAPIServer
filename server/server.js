9946d864f862fe2084224ead44a7c59a21fe22ea
Stephen Merki <stephen.merki232@gmail.com>
2023-06-13 11:17:17 -0500

    added server logic that will check for ssl certificate.

--- server/server.js
+++ server/server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
 const path = require('path');
 const routes = require('./routes');
+
+
+const options = {
+  pfx: fs.readFileSync('../../ssl/anser-wildcard-2023.pfx'),
+  passphrase: process.env.SSL_PASSWORD
+};
 
 const PORT = process.env.PORT || 80;
 
@@ -12,6 +20,8 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '../client/build')));
 app.use(routes);
 
-app.listen(PORT, () => {
-  console.log(`🌍 Now listening on localhost:${PORT}`);
+https.createServer(options, (req, res) => {
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+  });
 });
