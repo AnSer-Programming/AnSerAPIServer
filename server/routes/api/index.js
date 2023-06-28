0e3edcc43d8d4943528314009c826d41a4cf4845
Stephen Merki <stephen.merki232@gmail.com>
2023-06-28 13:20:13 -0500

    set up connection from the API server to the database server.

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const timeConverterRoute = require('./time-converter');
 const schedulerRoute = require('./scheduler');
 const billy = require('./Billy');
+const onTimeRoute = require('./OnTime');
 
 router.use('/timeConverter', timeConverterRoute);
 router.use('/Billy', billy);
 router.use('/scheduler', schedulerRoute);
+router.use('/OnTime', onTimeRoute);
 
 module.exports = router;
