9803305a0658588dd9f30600f9a1e85a2755575d
Stephen Merki <stephen.merki232@gmail.com>
2023-09-20 16:23:59 -0500

    Adjusted the images so that they would have the maxWidth of 100% to limit overflow.

--- client/src/components/WalkThrough/VesselList.tsx
+++ client/src/components/WalkThrough/VesselList.tsx
@@ -12,30 +12,30 @@ const VesselListWalkThrough = () => {
         <ol>
           <li>
             First click the purple button "Click Here To Update Vessel Contacts."<br /><br />
-            <img src={StepOne} /><br /><br /><br />
+            <img src={StepOne} style={{maxWidth: "100%"}} /><br /><br /><br />
           </li>
           <li>
             Next you will need to select the "Miscellanous contact" and then you will be able to access the website for updating the Vessel List.<br /><br />
-            <img src={StepTwoAndThree} /><br /><br /><br />
+            <img src={StepTwoAndThree} style={{maxWidth: "100%"}} /><br /><br /><br />
           </li>
           <li>
             Click on the Personnel drop down list and select the account you want to update. Account 38 is a sandbox that you can use to learn how the web tool works.<br /><br />
-            <img src={StepFour} style={{border: "2px solid white"}} /><br /><br /><br />
+            <img src={StepFour} style={{border: "2px solid white", maxWidth: "100%"}} /><br /><br /><br />
           </li>
           <li>
             After you have selected the account you want to update, you will be in the review mode for the Vessel List. <br />
             In order to edit the list you will need to click the enable editing button.<br /><br />
-            <img src={StepFive} style={{border: "2px solid white"}} /><br /><br /><br />
+            <img src={StepFive} style={{border: "2px solid white", maxWidth: "100%"}} /><br /><br /><br />
           </li>
           <li>
             In the editting screen you can change the Vessel name and the Contact associated with that Vessel. You can also add and/or delete rows from the Vessel list. <br /><br />
-            <img src={StepSix} style={{border: "2px solid white"}} /><br /><br /><br />
+            <img src={StepSix} style={{border: "2px solid white", maxWidth: "100%"}} /><br /><br /><br />
           </li>
           <li>
             In order to add a row, you will need to click the "Add A Blank Row" and this will add a blank row below the Unlisted Vessel entry. <br />
             To delete a row, you will click the "Delete Row" button to the right hand side of the row that you no longer need.<br />
             When you are done editing the Vessel List, click the "Save and View" button. This will then send you back to the review screen for the Vessel List.<br /><br />
-            <img src={WithBlankRow} style={{border: "2px solid white"}} /><br /><br /><br />
+            <img src={WithBlankRow} style={{border: "2px solid white", maxWidth: "100%"}} /><br /><br /><br />
           </li>
         </ol>
       </div>
