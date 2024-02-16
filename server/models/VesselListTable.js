9b2331a3c7c7ac45672fceb5797a2a69ced9d760
Stephen Merki <stephen.merki232@gmail.com>
2024-02-16 10:51:49 -0600

    Included stored procedure that will bring in more data for teh call info API

---
+++ server/models/VesselListTable.js
@@ -0,0 +1,39 @@
+const { DataTypes, Model } = require('sequelize');
+const sequelize = require('../config/connection');
+
+try {
+  class VesselListTable extends Model {}
+  VesselListTable.init(
+    {
+      index: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      vessel_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      contact_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      account_num: {
+        type: DataTypes.BIGINT,
+        allowNull: false,
+      }
+    },
+    {
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'vesselList',
+    }
+  );
+  
+  module.exports = VesselListTable;
+} catch(err) {
+  console.log(`Model Error: ${ err }`);
+}
