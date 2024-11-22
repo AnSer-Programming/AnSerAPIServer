04c6b80f78e7e0e0766a72bf6b174ea01d55e938
Stephen Merki <stephen.merki232@gmail.com>
2024-11-22 10:45:17 -0600

    Added martincrump to the davis and crump email group. Moved Client Info to below Home in the navigation menu. Client Info is not a web tool but rather an entire web site.

--- server/node-mailer/emailToClient/SendCSVToDavisAndCrump.js
+++ server/node-mailer/emailToClient/SendCSVToDavisAndCrump.js
@@ -32,9 +32,9 @@ const sendEmail = (data) => {
   async function main() {
     const info = await transporter.sendMail({
       from: `${process.env.EMAIL_USER_API}`, // sender address
-      to: "rnecaise@myomnitech.com; robert.cain@daviscrump.com; Trevor.Rockstad@daviscrump.com; latrisha.crump@daviscrump.com; leads@daviscrump.com", // list of receivers
+      to: "rnecaise@myomnitech.com; robert.cain@daviscrump.com; Trevor.Rockstad@daviscrump.com; latrisha.crump@daviscrump.com; martincrump@daviscrump.com; leads@daviscrump.com", // list of receivers
       cc: [
-        'joe@anser.com', 'stephenm@anser.com'
+        'stephenm@anser.com'
       ],
       // to: "stephenm@anser.com",
       subject: "Your Daily Report", // Subject line
